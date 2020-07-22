2e6a8ef61c59a70850ada5e434c40d5d2f452c11
Inho Kim <vrladlsghv@naver.com>
2020-07-22 14:55:20 +0900

    [Feat] (F) 로그인 로직 필상이가 만든거 붙여야함, Closes S03P12B101-36

--- ssalog_front/src/App.vue
+++ ssalog_front/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <router-link :to="{ name: 'SignUp' }">회원가입</router-link>
+    <router-link :to="{ name: 'Login' }">로그인</router-link>
     <router-view />
   </v-app>
 </template>
