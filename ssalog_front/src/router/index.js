095dc4ba72c4674ce248a02e918467545692b212
uutaein <uutaein@gmail.com>
2020-07-23 14:09:41 +0900

    [style] login 대충 틀잡음 그런데 margin 이랑 padding 이 잘 안먹히는데 이부분은 공부를 해야될것 같음

--- ssalog_front/src/router/index.js
+++ ssalog_front/src/router/index.js
@@ -2,10 +2,16 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import SignUp from "@/views/Account/SignUp.vue";
 import Login from "@/views/Account/Login.vue";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
   {
     path: "/SignUp",
     name: "SignUp",
--- ssalog_front/src/views/Account/Login.vue
+++ ssalog_front/src/views/Account/Login.vue
@@ -1,16 +1,29 @@
 <template>
-  <v-form ref="form" lazy-validation>
-    <v-text-field v-model="loginData.username" :counter="10" label="아이디" required></v-text-field>
-
-    <v-text-field
-      type="password"
-      v-model="loginData.password"
-      label="비밀번호"
-      required
-    ></v-text-field>
-
-    <v-btn color="success" class="mr-4" @click="onSubmit()">로그인</v-btn>
-  </v-form>
+  <v-img src="@/assets/images/intro-visual-bg01.jpg">
+    <v-container>
+      <v-row>
+        <v-col cols="12" lg="2" offset-lg="3">
+          <v-card class="mx-auto" height="40vh">
+            <v-form ref="form" lazy-validation>
+              <v-text-field
+                v-model="loginData.username"
+                :counter="10"
+                label="아이디"
+                required
+              ></v-text-field>
+              <v-text-field
+                type="password"
+                v-model="loginData.password"
+                label="비밀번호"
+                required
+              ></v-text-field>
+              <v-btn color="success" class="mr-4" @click="onSubmit()">로그인</v-btn>
+            </v-form>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-img>
 </template>
 
 <script>
@@ -38,4 +51,8 @@ export default {
   }
 };
 </script>
-<style></style>
+<style scoped>
+.p_page {
+  margin-top: "200vh";
+}
+</style>
