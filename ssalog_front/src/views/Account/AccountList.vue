d114bf9c77d7a2fbbea410d361dd1e54316257bf
Inho Kim <vrladlsghv@naver.com>
2020-07-25 23:28:16 +0900

    [feature] (f) making 검색, 리스트(사용자, 문제)

--- ssalog_front/src/views/Account/AccountList.vue
+++ ssalog_front/src/views/Account/AccountList.vue
@@ -9,8 +9,18 @@ export default {
   name: "AccountList",
   data() {
     return {
-      search: this.$route.params.search
+      page: 0,
+      search: this.$route.query.search,
+      users: []
     };
+  },
+  mounted() {
+    if (!this.search) {
+      // 전체 user 조회
+      this.search = "검색어가 없으므로 걍 전체리스트한다 ㅇㅋ?";
+    } else {
+      // 닉 기반으로 조회
+    }
   }
 };
 </script>
