300afb0726f7411364702e7337c870d0516f5e63
정성오 <wnw1102@naver.com>
2020-07-26 19:06:53 +0900

    [feature]_B_post작업setting

---
+++ ssalog_backend/src/main/java/com/ssalog/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.ssalog.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.ssalog.dto.Post;
+
+public interface PostRepository extends MongoRepository<Post,String>{
+
+}
---
+++ ssalog_backend/src/main/java/com/ssalog/service/PostService.java
@@ -0,0 +1,5 @@
+package com.ssalog.service;
+
+public interface PostService {
+
+}
