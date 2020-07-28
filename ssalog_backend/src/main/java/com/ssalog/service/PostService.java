c8f65a36f47900e996658af3727d0094e4d5bfc3
정성오 <wnw1102@naver.com>
2020-07-28 10:39:14 +0900

    [feature]_B_p105_포스팅_조건별검색

--- ssalog_backend/src/main/java/com/ssalog/service/PostService.java
+++ ssalog_backend/src/main/java/com/ssalog/service/PostService.java
@@ -1,5 +1,10 @@
 package com.ssalog.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 import com.ssalog.dto.Comment;
 import com.ssalog.dto.Post;
 
@@ -9,4 +14,7 @@ public interface PostService {
 	public Post read_post(String post_pk);
 	public void delete_post(String post_pk);
 	public void update_post(Post p);
+	public Page<Post> select_by_problemid(String problemid, PageRequest pageable);
+	public Page<Post> select_by_problemname(String problemname, PageRequest pageable);
+	public Page<Post> select_by_keyword(String keyword, PageRequest pageable);
 }
