c446fc828da47ea8ad01b01018fe95f52b91166b
정성오 <wnw1102@naver.com>
2020-08-06 22:31:09 +0900

    [feat/back] 댓글기능 처리

--- ssalog_backend/src/main/java/com/ssalog/service/CommentService.java
+++ ssalog_backend/src/main/java/com/ssalog/service/CommentService.java
@@ -9,7 +9,7 @@ public interface CommentService {
 	public int write_comment(String post_pk,String comment,String username);
 	public List<Comment> read_comment(String Scoring);
 	public void delete_comment(String id);
-	public void update_comment(Comment comment,String id);
+	public void update_comment(Comment comment,String id, String username);
 	public void write_subcomment(String id, Comment comment);
 	public void update_subcomment(String id, Comment comment);
 }
