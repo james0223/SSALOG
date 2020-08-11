4ed121599aadb588551e6c6f1c728c379586b210
정성오 <wnw1102@naver.com>
2020-08-11 14:50:27 +0900

    [feat/back] fix2

--- ssalog_backend/src/main/java/com/ssalog/repository/AccountRepository.java
+++ ssalog_backend/src/main/java/com/ssalog/repository/AccountRepository.java
@@ -12,12 +12,8 @@ import com.ssalog.dto.AccountSub;
 // CrudRepository는 관리되는 엔티티 클래스에 대해 정교한 CRUD 기능을 제공한다. 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
     Account findByUsername(String username);	// user 검색
-    Account findByEmail(String email);			// email 검색
-    Account findBybirthday(String birthday);	// 생일 검색
     Account findByNickname(String nickname);	// 닉네임 검색
     Long deleteByUsername(String username);		// user 삭제 (admin 사용자만)
-    Account findByUsernameAndEmail(String username, String email); // 유저이름과 이메일을 이용한 사용자 찾기
-    Account findByUsernameAndAnswer(String username, String answer); // 유저이름과 답변을 이용한 사용자 찾기
     Page<AccountSub> findByNicknameLike(String nickname, Pageable pageable);
 }
 
