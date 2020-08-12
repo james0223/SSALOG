5e1c104fa77015c61de3fd0480425e7a81bdaae0
uutaein <uutaein@gmail.com>
2020-08-12 18:27:33 +0900

    [proto/front] param 조정과 catch error 수정 필요

--- ssalog_front/src/views/Group/GroupList.vue
+++ ssalog_front/src/views/Group/GroupList.vue
@@ -9,9 +9,35 @@
                 <v-card-title>
                   나의 그룹
                   <v-spacer></v-spacer>
-                  <v-btn tile outlined class="ma-2" color="primary">
+                  <v-btn
+                    tile
+                    outlined
+                    class="ma-2"
+                    color="primary"
+                    @click.stop="createGroupDialog = true"
+                  >
                     <v-icon>mdi-plus</v-icon>그룹 만들기
                   </v-btn>
+                  <v-dialog v-model="createGroupDialog" max-width="700">
+                    <v-card class="px-5">
+                      <v-card-title class="headline">그룹 만들기</v-card-title>
+                      <v-text-field class="px-5" label="그룹 이름" v-model="createGroupName">
+                        <v-icon slot="prepend">mdi-format-title</v-icon>
+                      </v-text-field>
+                      <v-text-field class="px-5" label="그룹 한마디" v-model="createGroupIntro">
+                        <v-icon slot="prepend">mdi-chat-processing</v-icon>
+                      </v-text-field>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="secondary" text @click="createGroupDialog = false">
+                          취소
+                        </v-btn>
+                        <v-btn color="secondary" text @click="makeGroup">
+                          그룹 만들기
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
                 </v-card-title>
                 <v-card-text class="pt-4">
                   <v-subheader>내가 가입된 그룹</v-subheader>
@@ -53,6 +79,8 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "GroupList",
   data() {
@@ -92,9 +120,27 @@ export default {
           GroupName: "호다닥"
         }
       ],
-      searchGroup: []
+      searchGroup: [],
+      // 그룹 가입 관련 data
+      createGroupDialog: false,
+      createGroupName: "",
+      createGroupIntro: ""
     };
-  }
+  },
+  methods: {
+    async makeGroup() {
+      try {
+        const res = await this.$http.post(`${this.ServerURL}/user/grouping/make_group`, {
+          groupname: this.createGroupName,
+          groupdesc: this.createGroupIntro
+        });
+      } catch (err) {
+        // 이미 그룹이 존재함으로써 받는 에러코드
+        console.error(err);
+      }
+    }
+  },
+  computed: mapState(["ServerURL"])
 };
 </script>
 
