eac19598c9a326ca297803aeac377556163f43f0
정성오 <wnw1102@naver.com>
2020-08-14 04:24:45 +0900

    [feat/back] resolves S03P13B101-124 resolves S03P13B101-119

--- ssalog_backend/src/main/java/com/ssalog/repository/PostSubRepository.java
+++ ssalog_backend/src/main/java/com/ssalog/repository/PostSubRepository.java
@@ -13,6 +13,7 @@ import com.ssalog.dto.Scrapedpost;
 
 public interface PostSubRepository extends MongoRepository<Post,String>{
 	public Page<PostSub> findByProblemid(String problemid, Pageable pageable);
+	public Page<PostSub> findByProblemidAndLanguage(String problemid, String language,Pageable pageable);
 	public Page<PostSub> findByUsername(String username, Pageable pageable);
 	public Page<PostSub> findByProblemnameLike(String problemname, Pageable pageable);
 	public List<PostSub> findByUsernameAndProblemid(String username, String problemid);
