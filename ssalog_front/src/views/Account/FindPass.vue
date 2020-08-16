435d6b3a6b9e77d8108fe9d41539e2cd4f4705f6
username <vrladlsghv@naver.com>
2020-08-16 16:39:16 +0900

    [fix/front] 언어DB수정관련, 비번찾기 수정

--- ssalog_front/src/views/Account/FindPass.vue
+++ ssalog_front/src/views/Account/FindPass.vue
@@ -1,49 +1,21 @@
 <template>
   <v-container my-16>
-    <v-card flat class="mx-auto" max-width="50vw">
-      <v-card-title class="title font-weight-regular justify-space-between">
+    <v-card color="transparent" flat class="mx-auto" max-width="50vw">
+      <v-card-title class="title font-weight-regular">
         <h1>비밀번호 찾기</h1>
       </v-card-title>
-
-      <v-window v-model="step">
-        <v-window-item :value="1">
-          <v-card-text class="pb-0">
-            <span class="caption grey--text text--darken-1"
-              >가입시 등록한 이메일을 입력해주세요</span
-            >
-            <v-text-field label="이메일" v-model="account.username"></v-text-field>
-          </v-card-text>
-        </v-window-item>
-        <v-window-item :value="2">
-          <v-card-text class="pb-0">
-            {{ account.username }} 으로 임시 비밀번호가 전송되었습니다. 다시 로그인해주세요
-          </v-card-text>
-        </v-window-item>
-        <!-- <v-window-item :value="2">
-          <v-card-text class="pb-0">
-            <span class="caption grey--text text--darken-1"
-              >{{ account.username }} 으로 발송된 인증코드를 입력해주세요</span
-            >
-            <v-text-field label="인증코드" v-model="account.code"></v-text-field>
-          </v-card-text>
-        </v-window-item>
-
-        <v-window-item :value="3">
-          <v-card-text class="pb-0">
-            <span class="caption grey--text text--darken-1">새로운 비밀번호를 입력해주세요</span>
-            <v-text-field
-              type="password"
-              label="새 비밀번호"
-              v-model="account.password"
-            ></v-text-field>
-            <v-text-field
-              type="password"
-              label="비밀번호 확인"
-              v-model="account.passwordCheck"
-            ></v-text-field>
-          </v-card-text>
-        </v-window-item> -->
-      </v-window>
+      <v-card-text v-if="step === 1" class="pb-0">
+        <span class="caption grey--text text--darken-1">가입시 등록한 이메일을 입력해주세요</span>
+        <v-text-field
+          @keypress.enter="findPassReq()"
+          label="이메일"
+          v-model="username"
+        ></v-text-field>
+        <v-progress-linear v-show="loading" indeterminate color="primary"></v-progress-linear>
+      </v-card-text>
+      <v-card-text v-if="step === 2" class="pb-0">
+        <h3>{{ username }} 으로 임시 비밀번호가 전송되었습니다. 다시 로그인해주세요</h3>
+      </v-card-text>
       <v-card-text class="py-0">
         <small class="red--text">{{ errorMsg }}</small>
       </v-card-text>
@@ -72,81 +44,35 @@ export default {
   data() {
     return {
       step: 1,
-      account: {
-        username: null,
-        passwordCheck: null,
-        password: null,
-        code: null
-      },
-      answer: null,
-      errorMsg: null
+      username: null,
+      errorMsg: null,
+      loading: false
     };
   },
   methods: {
     ...mapMutations(["ShowAlert"]),
-    updatePasswordReq() {
+    async findPassReq() {
       this.errorMsg = null;
-      if (!this.account.password || !this.account.passwordCheck) {
-        this.errorMsg = "비밀번호와 확인문자를 입력해주세요";
-      } else if (this.account.password !== this.account.passwordCheck) {
-        this.errorMsg = "비밀번호 확인문자가 일치하지 않습니다.";
+      if (!this.username) {
+        this.errorMsg = "이메일을 입력해주세요";
       } else {
-        axios
-          .put(`${this.$store.state.ServerURL}/user/change_pw`, null, {
+        try {
+          this.loading = true;
+          const { data } = await axios.get(`${this.$store.state.ServerURL}/newuser/find_pw`, {
             params: {
-              password: this.account.password
+              username: this.username
             }
-          })
-          .then(({ data }) => {
-            if (data) {
-              this.ShowAlert({
-                flag: true,
-                msg: "비밀번호 변경 성공! 다시 로그인해주세요",
-                color: "info"
-              });
-              setTimeout(() => {
-                this.ShowAlert({ flag: false, msg: "" });
-              }, 2000);
-              this.$router.push({ name: "Home" });
-            } else {
-              this.errorMsg = "서버에러";
-            }
-          })
-          .catch(err => {
-            console.error(err);
-            this.errorMsg = "서버에러";
           });
-      }
-    },
-    findPassReq() {
-      this.errorMsg = null;
-      if (this.step === 1) {
-        if (!this.account.username) {
-          this.errorMsg = "이메일을 입력해주세요";
-        } else {
-          // 수정필요
-          axios
-            .get(`${this.$store.state.ServerURL}/newuser/수정필요`, {
-              params: {
-                username: this.account.username
-              }
-            })
-            .then(({ data }) => {
-              if (data.result) {
-                this.step = 2;
-                this.answer = data.정답;
-              } else {
-                this.errorMsg = "유효하지 않은 이메일입니다.";
-              }
-            })
-            .catch(err => console.error(err));
+          if (data) {
+            this.step = 2;
+          } else {
+            this.errorMsg = "유효하지 않은 이메일입니다.";
+          }
+        } catch (e) {
+          console.error(e);
+          this.errorMsg = "서버응답이 없습니다. 다시 시도해주세요";
         }
-      } else if (this.step === 2) {
-        if (this.account.code !== this.answer) {
-          this.errorMsg = "인증코드가 일치하지 않습니다.";
-        } else {
-          this.step = 3;
-        }
+        this.loading = false;
       }
     }
   }
