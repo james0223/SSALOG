646d7b63b179826036d54ad8f08d851ea9ec31a4
philsang <gofeel8@hanmail.net>
2020-08-17 05:54:24 +0900

    [feat/front]디폴트수정폼완성

--- ssalog_front/src/views/SSALOG/Article/WriteLog.vue
+++ ssalog_front/src/views/SSALOG/Article/WriteLog.vue
@@ -401,7 +401,7 @@ export default {
             }
           });
           console.log(res)  ;
-          if(res.data==="null" || res.data==="fail"){
+          if(res.data===null || res.data==="fail"){
             res.data = "<h2>접근방법</h2><hr><p></p><h2>코드분석</h2><hr>";
           }
         } catch (e) {
