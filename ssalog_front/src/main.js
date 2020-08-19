ea9e22a78062e81cf3c8f9f64d45c12bb1e8a249
uutaein <uutaein@gmail.com>
2020-08-19 19:16:35 +0900

    [fix/front] 확인을 위해서 콘솔에 올림

--- ssalog_front/src/main.js
+++ ssalog_front/src/main.js
@@ -73,6 +73,7 @@ axios.interceptors.response.use(
             refreshToken: `Bearer ${refreshToken}`
           }
         });
+        console.log("재요청 결과", res);
         if (res.success === true) {
           store.commit("TOKEN", { accessToken: res.data.accessToken, refreshToken });
           axios.defaults.headers.common.accessToken = res.data.accessToken;
